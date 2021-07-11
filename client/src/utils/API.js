7da1115c3baae67f60c711b7fed860ac2a504af7
amandardz <amandardz15@gmail.com>
2021-07-11 17:25:57 -0500

    adding axios and api request

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,1 +1,8 @@
+import axios from 'axios';
+require('dotenv').config();
 
+export default {
+  getBooks: function(query) {
+    return axios.get('https://www.googleapis.com/books/v1/volumes?q=' + query + '&printType=books&orderBy=relevance&key=' + process.env.REACT_APP_API_KEY)
+  }
+}
