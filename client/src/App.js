1e4199a649971d9bd2ecf806660ae0ef5bc4c7b4
amandardz <amandardz15@gmail.com>
2021-07-15 19:15:34 -0500

    adding router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Search from './pages/Search'
+import Saved from "./pages/Saved";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar/index";
+
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div>
       <Navbar />
       <Header />
-      <Search />
-    </div>
+      <Switch>
+        <Route exact path={['/', '/books']}>
+          <Search />
+        </Route>
+        <Route exact path='/saved'>
+          <Saved />
+        </Route>
+      </Switch>
+      </div>
+    </Router>
   );
 }
 
