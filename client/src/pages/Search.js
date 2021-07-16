b3698470c47af99da750fde265242a8bd95c4cd8
amandardz <amandardz15@gmail.com>
2021-07-15 19:17:58 -0500

    adding search functionality

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,22 +1,28 @@
 import React, {useState} from 'react';
 import Container from '../components/Container';
 import Searchbar from '../components/Searchbar';
-import Card from '../components/Card'
+import SearchCard from '../components/SearchCard';
 import API from '../utils/API';
 
 function Search() {
   const [bookSearch, setBookSearch] = useState('');
-  const [searchResults, setSearchResults] = useState();
+  const [searchResults, setSearchResults] = useState([]);
 
   const handleInputChange = event => {
     const { value } = event.target;
     setBookSearch(value);
   }
+
+  const saveBook = (book) => {
+    API.saveBook(book).then((res) => {
+      console.log(res);
+    });
+  };
  
   const handleFormSubmit = event => {
     event.preventDefault();
     API.getBooks(bookSearch)
-    .then(res => {setSearchResults(res.data)})
+    .then((res) => setSearchResults(res.data.items))
     .catch(err => console.log(err))
   }
 
@@ -25,7 +31,31 @@ function Search() {
     <>
       <Searchbar value={bookSearch} onChange={handleInputChange} onClick={handleFormSubmit}/>
       <Container>
-        <Card/>
+        {searchResults.map(
+          ({
+            id, 
+            volumeInfo: {
+              title, 
+              authors,
+              description,
+              infoLink,
+              imageLinks: { thumbnail },
+            },
+          }) => {
+          return(
+            <SearchCard 
+              key={id}
+              title={title}
+              authors={authors}
+              description={description}
+              thumbnail={thumbnail}
+              link={infoLink}
+              onSave={() => {
+                saveBook({ title, authors, description, image: thumbnail, link: infoLink});
+              }}
+            />
+          );
+          })}
       </Container>
     </>
   )
