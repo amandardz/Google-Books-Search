5caa69208d112c7024b7a77a0062525b5b764484
amandardz <amandardz15@gmail.com>
2021-07-15 19:24:41 -0500

    adding mongo methods

--- controllers/booksControllers.js
+++ controllers/booksControllers.js
@@ -1,33 +1,27 @@
-const db = require('../models')
+const Book = require('../models/book')
 
 module.exports = {
   findAll: function(req, res) {
-    db.Book
+    Book
       .find(req.query)
       .sort({ date: -1 })
-      .then(dbModel =>  res.json(dbModel))
+      .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Book
+    Book
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Book
+    Book
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  update: function(req, res) {
-    db.Book
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
   remove: function(req, res) {
-    db.Book
+    Book
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
