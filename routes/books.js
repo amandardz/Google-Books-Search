f57777b4ed3beea975117bb3af11e99f81515e45
amandardz <amandardz15@gmail.com>
2021-07-15 19:25:35 -0500

    modifying routes

--- routes/books.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const booksController = require('../controllers/booksControllers')
-
-// Matches with "/api/books"
-router.route("/")
-  .get(booksController.findAll);
-
-
-  module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
-const router = require("express").Router();
-const bookRoutes = require("./books");
+const router = require('express').Router();
+const apiRoutes = require('./api');
 
-// Book routes
-router.use("/books", bookRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
